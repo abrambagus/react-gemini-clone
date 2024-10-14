941b1d0f92de8955cc2544605ddc2ba9515682dc
abram <abrambagus@gmail.com>
2024-10-14 15:42:16 +0700

    add generative AI function

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,34 +1,67 @@
+import { useContext } from 'react'
 import { assets } from '../../assets/assets'
 import './Main.css'
+import { Context } from '../../context/Context'
 
 const Main = () => {
-  return (
-    <div className='main'>
-        <div className="nav">
-            <p>Gemini</p>
-            <img src={assets.user_icon} alt="" />
-        </div>
-        <div className="main-container">
-            <div className="greet">
-                <p className='hello'><span>Hello, User</span></p>
-                <p>How can I help?</p>
+
+    const {onSent, recentPrompt, showResult, loading, resultData, setInput, input} = useContext(Context)
+
+    return (
+        <div className='main'>
+            <div className="nav">
+                <p>Gemini</p>
+                <img src={assets.user_icon} alt="" />
             </div>
-        </div>
-        <div className="main-bottom">
-            <div className="search-box">
-                <input type="text" placeholder='Enter a promt here'/>
-                <div>
-                    <img src={assets.gallery_icon} alt="" />
-                    <img src={assets.mic_icon} alt="" />
-                    <img src={assets.send_icon} alt="" />
+            <div className="main-container">
+
+                {!showResult ? (
+                      <div className="greet">
+                        <p className='hello'><span>Hello, User</span></p>
+                        <p>How can I help?</p>
+                    </div>
+                ) : (
+                    <div className='result'>
+                        <div className="result-title">
+                            <img src={assets.user_icon} alt="" />
+                            <p>{recentPrompt}</p>
+                        </div>
+                        <div className="result-data">
+                            <img src={assets.gemini_icon} alt="" />
+                            {loading ? (
+                                <div className="loader">
+                                    <hr />
+                                    <hr />
+                                    <hr />
+                                </div>
+                            ) : (
+                                <p dangerouslySetInnerHTML={{__html: resultData}}></p>
+                            )}
+                            
+                        </div>
+                    </div>
+                )}
+            </div>
+            <div className="main-bottom">
+                <div className="search-box">
+                    <input 
+                        onChange={(e) => setInput(e.target.value)} 
+                        value={input} 
+                        type="text" 
+                        placeholder='Enter a promt here'
+                    />
+                    <div>
+                        <img src={assets.gallery_icon} alt="" />
+                        <img src={assets.mic_icon} alt="" />
+                        <img onClick={() => onSent()} src={assets.send_icon} alt="" />
+                    </div>
                 </div>
+                <p className="bottom-info">
+                    Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps
+                </p>
             </div>
-            <p className="bottom-info">
-                Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps
-            </p>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Main
